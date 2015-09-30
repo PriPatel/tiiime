ba92f6f41306e3e2fb9e16f3022a8de4e27aad55
PriPatel <priyanka.patel@unrulygroup.com>
2015-09-30 21:20:11 +0100

    Add title into contentContainer

--- tiiime.js
+++ tiiime.js
@@ -2,10 +2,18 @@ var events = [
 {
 	"image":"http://placehold.it/400x200",
 	"title":"Grade Abe"
+},
+{
+	"image":"http://placehold.it/300x200",
+	"title":"Grader Abe"
 }];
 
 var scrollBarContainer = $('#scroll-bar-container');
 scrollBarContainer.slider();
 var contentContainer = $('#content-container');
-var image = $('<img src="' + events[0].image + '">');
+var image = $('<img src="' + events[1].image + '">');
+var title = $('<p>' + events[1].title + '</p>');
 contentContainer.append(image);
+contentContainer.append(title);
+
+
