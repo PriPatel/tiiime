e5dee3f50d35751d0577d8363e44e02afc25ea11
PriPatel <priyanka.patel@unrulygroup.com>
2015-10-02 01:24:16 +0100

    fuck you

--- tiiime.js
+++ tiiime.js
@@ -1,16 +1,85 @@
 var events = [
 {
-	"image":"http://placehold.it/400x200",
-	"title":"Grade Abe"
+	"image":"piccys/UK.png",
+	"title":"London HQ established",
+	"date":"January 2006"
 },
 {
-	"image":"http://placehold.it/300x200",
-	"title":"Grader Abe"
+	"image":"piccys/aus.png",
+	"title":"Re-seller agreement in Sydney",
+	"date":"2009"
 },
 {
-	"image":"http://placehold.it/500x400",
-	"title":"Gradest Abe"
-}];
+	"image":"piccys/paris.png",
+	"title":"First international office opens in Paris",
+	"date":"March 2010"
+},
+{
+	"image":"piccys/UK.png",
+	"title":"Unruly HQ moves from Truman Brewery to Fashion St",
+	"date":"January 2011"
+},
+{
+	"image":"piccys/US.png",
+	"title":"New York and San Francisco offices open",
+	"date":"February 2011"
+},
+{
+	"image":"piccys/norway.png",
+	"title":"Stockholm office opens",
+	"date":"June 2011"
+},
+{
+	"image":"piccys/gemrnay.png",
+	"title":"Hamburg office opens",
+	"date":"February 2012"
+},
+{
+	"image":"piccys/US.png",
+	"title":"Chicago office opens",
+	"date":"April 2012"
+},
+{
+	"image":"piccys/US.png",
+	"title":"LA office opens",
+	"date":"August 2012"
+},
+{
+	"image":"piccys/US.png",
+	"title":"Chicago office opens",
+	"date":"April 2012"
+},
+{
+	"image":"piccys/UK.png",
+	"title":"Unruly HQ moves from Fashion St to Princelet St",
+	"date":"June 2012"
+},
+{
+	"image":"piccys/sweden.png",
+	"title":"Oslo office opens",
+	"date":"September 2013"
+},
+{
+	"image":"piccys/APAV.png",
+	"title":"APAC Regional HQ opens in Singapore",
+	"date":"March 2014"
+},
+{
+	"image":"piccys/southkorea.png",
+	"title":"Enter S Korea via Re-seller",
+	"date":"October 2014"
+},
+{
+	"image":"piccys/dubai.png",
+	"title":"Enter Dubai via re-seller",
+	"date":"January 2015"
+},
+{
+	"image":"piccys/aus.png",
+	"title":"Sydney office opens ",
+	"date":"April 2015"
+},
+];
 
 function onSlide(event,ui) {
 	var scalar = ui.value / 100.01;
@@ -39,9 +108,11 @@ for (var i = 0; i < events.length; i++) {
 	eventContainers.push($('<div class="event-content-container"></div>'));
 	var image = $('<img src="' + events[i].image + '">');
 	var title = $('<p>' + events[i].title + '</p>');
+	var date = $('<p>' + events[i].date + '</p>');
 
 	eventContainers[i].append(image);
     eventContainers[i].append(title);
+    eventContainers[i].append(date);
     contentContainer.append(eventContainers[i]);
 
 
